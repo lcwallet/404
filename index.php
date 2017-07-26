efa55573ec32ae98687841778811882ba82a95d8
azriq <azriqfawani.jamaludin@bizedge.com>
2017-07-26 11:06:12 +0800

    add initial push

---
+++ index.php
@@ -0,0 +1,56 @@
+ <head>
+        <meta charset="UTF-8">
+        <title>TEP login/signup</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <link rel="stylesheet" href="css/404.css">
+         <link rel="stylesheet" href="css/404-animate.css">
+
+    </head>
+
+   <body class="fw-404">   
+
+   
+
+    <div class="full-width p-t-40"> 
+    <!-- Start Container-->    
+    <div class="container content-inner">
+      <div class="row content">        
+        <div class="col-md-12">
+          <!-- Start 404 TEXT-->        
+          <section class="main main-404">
+            <h2 class="text404 animated In bounceIn">         
+              <span class="text404-cut"><span class="word1">Oops!
+</span> <span class="word2"></span> <span class="word3"></span> <span class="word4"></span> <span class="word5"></span> <span class="word6"></span> <span class="word7"></span> <span class="word8"></span> <span class="word9"></span> <span class="word10"></span> <span class="word11"></span> <span class="word12"></span> <span class="word13"></span> <span class="word14"></span> <span class="word15"></span> </span>         
+              <span class="text404-mid">Error 404!
+              </span>         
+              <span class="text404-cut"><span class="word1">Oops!
+</span> <span class="word2"></span> <span class="word3"></span> <span class="word4"></span> <span class="word5"></span> <span class="word6"></span> <span class="word7"></span> <span class="word8"></span> <span class="word9"></span> <span class="word10"></span> <span class="word11"></span> <span class="word12"></span> <span class="word13"></span> <span class="word14"></span> <span class="word15"></span> </span>       
+            </h2>        
+          </section> 
+          <!-- End 404 TEXT-->
+          <!-- Main TEXT-->       
+          <section class="desc-404 animated In fadeInDown">    
+            <h4>The page you're looking for no longer exists. Would you like to go to <a href="#">homepage</a> instead?</h4>
+          </section>
+          <!-- End Main TEXT-->
+          <!-- Search form-->
+          <section class="search-404 animated In fadeInUp">
+            <form id="search-404" action="/search" method="get">
+               <div>
+               <i class="search-icon-404 ion-icon ion-ios-search-strong"></i> 
+               <input type="text" placeholder="What are you looking for..." size="40" name="search">
+                      
+               </div>
+               
+            </form>
+      </section>
+          <!-- End Search form-->
+        </div> 
+      </div> 
+    </div>         
+    <!-- End Container -->
+    </div>                        
+    <!--Footer-->             
+    
+  
+</body>
